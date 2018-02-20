b27c60a51e86f3b99aaf295ef4f609133c2a5bb1
ralph kemp <ralphk3mp@gmail.com>
2018-02-20 23:59:10 +0000

    deleted users controller for now and will do tmorrow

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   resources :jobs
-
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-
+  devise_for :users
 end
