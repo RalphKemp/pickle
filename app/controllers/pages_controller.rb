1fc48b60958905cb393a6519722ddc98361d589b
ralph kemp <ralphk3mp@gmail.com>
2018-02-20 19:46:01 +0000

    linked dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to jobs_path
+    end
+  end
+
 end
