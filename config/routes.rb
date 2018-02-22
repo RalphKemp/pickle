1517f1e5d2f2219f1cf3d585ca94b594972be0bc
ralph kemp <ralphk3mp@gmail.com>
2018-02-22 01:11:40 +0000

    changed booking routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   resources :workers
-  resources :bookings, only: [:new]
+  resources :bookings, except: [:index]
   devise_for :users
 end
 
