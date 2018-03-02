ffce9776cc16abe3d875829f1ba4916c80286903
ralph kemp <ralphk3mp@gmail.com>
2018-03-02 18:28:21 +0000

    started experimenting with something but then realised it's stupid, might do it later

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,2 @@
 import "bootstrap";
+import picker from 'picker.js';
---
+++ app/javascript/picker.js
@@ -0,0 +1,6 @@
+// const formNumber = document.querySelectorAll('option');
+// const numberDiv = document.getElementById('number');
+// const value = numberDiv.innerHTML;
+
+
+
