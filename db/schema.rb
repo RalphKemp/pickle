e73a36766281c20ef6b4b63f2fdd1643ada02daa
ralph kemp <ralphk3mp@gmail.com>
2018-03-27 01:46:08 +0100

    jsdfjsfj

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327001647) do
+ActiveRecord::Schema.define(version: 20180327004446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 20180327001647) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false, null: false
     t.string "name"
     t.boolean "customer", default: true
     t.boolean "worker", default: false
