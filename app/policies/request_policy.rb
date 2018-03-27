8456d695fb0d5c81c2798626aca4e7a98f549fcc
ralph kemp <ralphk3mp@gmail.com>
2018-03-27 01:47:52 +0100

    tingd

--- app/policies/request_policy.rb
+++ app/policies/request_policy.rb
@@ -16,17 +16,17 @@ class RequestPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner_or_admin?
+    user_is_owner?
   end
 
   def destroy?
-    user_is_owner_or_admin?
+    user_is_owner?
   end
 
   private
 
-  def user_is_owner_or_admin?
-    record.user == user || user.admin
+  def user_is_owner?
+    record.user == user
   end
 
 end
